b655549cc3ebb29b266fa35169d4f9ddf6f1c65e
Robpol86 <robpol86@gmail.com>
2017-02-18 23:30:55 -0800

    Handling edge-case: COMPATIBILITY=false
    When updating the kernel with COMPATIBILITY=false in resize2fs,
    initramfs-rebuild will build for both kernels and the last one will
    remain as the only one in initramfs.gz regardless of the actual
    hardware. When updating the kernel the post-install hook is called for
    each kernel.
    
    Handing this by having initramfs-rebuild skip building on the wrong
    hardware. If COMPATIBILITY=true then both kernel's modules will be in
    the final image, and when false only the current hardware's modules will
    be present.
    
    Tweaking shell scripts for consistency and less lines as per examples in
    https://wiki.ubuntu.com/DashAsBinSh and
    http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html
    
    Putting some content in 404.rst instead of the dumb message I've had for
    so long.

--- docs/_static/initramfs-rebuild.sh
+++ docs/_static/initramfs-rebuild.sh
@@ -6,12 +6,30 @@
 
 # Remove splash from cmdline
 if grep -q '\bsplash\b' /boot/cmdline.txt; then
-    sed -i 's/ \?splash \?/ /' /boot/cmdline.txt
+  sed -i 's/ \?splash \?/ /' /boot/cmdline.txt
 fi
 
-# Rebuild only if needed.
+# Exit if not building kernel for this Raspberry Pi's hardware version.
 version="$1"
+current_version="$(uname -r)"
+case "${current_version}" in
+  *-v7+)
+    case "${version}" in
+      *-v7+) ;;
+      *) exit 0
+    esac
+  ;;
+  *+)
+    case "${version}" in
+      *-v7+) exit 0 ;;
+    esac
+  ;;
+esac
+
+# Exit if rebuild cannot be performed or not needed.
 [ -x /usr/sbin/mkinitramfs ] || exit 0
 [ -f /boot/initramfs.gz ] || exit 0
-lsinitramfs /boot/initramfs.gz |grep -q "/$version$" && exit 0
+lsinitramfs /boot/initramfs.gz |grep -q "/$version$" && exit 0  # Already in initramfs.
+
+# Rebuild.
 mkinitramfs -o /boot/initramfs.gz "$version"
--- docs/_static/resize2fs.sh
+++ docs/_static/resize2fs.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # Copy resize2fs, fdisk, and other kernel modules into initramfs image.
 # https://github.com/Robpol86/robpol86.com/blob/master/docs/_static/resize2fs.sh
 # Save as (chmod +x): /etc/initramfs-tools/hooks/resize2fs
-
-set -e  # Exit script if a command fails.
 
 COMPATIBILITY=true  # Set to false to skip copying other kernel's modules.
 
@@ -16,7 +14,7 @@ case "${1}" in
   prereqs)
     prereqs
     exit 0
-    ;;
+  ;;
 esac
 
 . /usr/share/initramfs-tools/hook-functions
@@ -27,16 +25,11 @@ copy_exec /sbin/fdisk /sbin
 # Raspberry Pi 1 and 2+3 use different kernels. Include the other.
 if ${COMPATIBILITY}; then
   case "${version}" in
-    *-v7+)
-      other_version="$(echo ${version} |sed 's/-v7+$/+/')"
-      ;;
-    *+)
-      other_version="$(echo ${version} |sed 's/+$/-v7+/')"
-      ;;
+    *-v7+) other_version="$(echo ${version} |sed 's/-v7+$/+/')" ;;
+    *+) other_version="$(echo ${version} |sed 's/+$/-v7+/')" ;;
     *)
       echo "Warning: kernel version doesn't end with +, ignoring."
       exit 0
-      ;;
   esac
   cp -r /lib/modules/${other_version} ${DESTDIR}/lib/modules/
 fi
