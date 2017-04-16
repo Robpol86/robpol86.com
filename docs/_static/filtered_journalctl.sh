bc044067fcb16a621d3a401c153edf21175cdf67
Robpol86 <robpol86@gmail.com>
2017-04-15 18:56:48 -0700

    Filtering out some kernel messages.
    These messages are itermittent and don't seem to indicate a real
    problem. No point in emailing me.

--- docs/_static/filtered_journalctl.sh
+++ docs/_static/filtered_journalctl.sh
@@ -13,9 +13,9 @@ journalctl -o json "$@" |while read -r line; do
     comm=
     container_name=
     hostname=
-    ident=
     message=
     pid=
+    syslog_ident=
     systemd_unit=
     timestamp=
     unit=
@@ -30,7 +30,7 @@ journalctl -o json "$@" |while read -r line; do
             _SYSTEMD_UNIT) systemd_unit="$value" ;;
             CONTAINER_NAME) container_name="$value" ;;
             MESSAGE) message="$value" ;;
-            SYSLOG_IDENTIFIER) ident="$value" ;;
+            SYSLOG_IDENTIFIER) syslog_ident="$value" ;;
             UNIT) unit="$value" ;;
         esac
     done < <(jq -j 'to_entries|map("\(.key)=\(.value)")|.[]|.+"\u0000"' <<< "$line")
@@ -48,12 +48,17 @@ journalctl -o json "$@" |while read -r line; do
     # Filter iptables warnings due to docker.
     if [ "$systemd_unit" == "firewalld.service" ] && [ "${message::24}" == "WARNING: COMMAND_FAILED:" ]; then
         if [[ "${message^^}" == *"DOCKER"* ]] || [[ "$message" == *" br-"* ]]; then continue; fi
-        if [[ "${message^^}" =~ $(echo '\b172.1(7|8).0.2\b') ]]; then continue; fi
+        if [[ "${message^^}" =~ $(echo "\b172.1(7|8).0.2\b") ]]; then continue; fi
+    fi
+
+    # Filter kernel messages.
+    if [ "$syslog_ident" == "kernel" ]; then
+        if [[ "$message" =~ "audit_printk_skb: "[0-9]+" callbacks suppressed" ]]; then continue; fi
     fi
 
     # Handle source column.
     if [ -z "$comm" ]; then
-        source="$ident"
+        source="$syslog_ident"
     else
         source="$comm[$pid]"
     fi
