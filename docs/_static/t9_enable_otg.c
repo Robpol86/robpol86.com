680aa780de5ca16f09dc6caeb3b6c2c0c916f0b6
Robpol86 <robpol86@gmail.com>
2021-11-12 13:45:15 -0800

    0xb7

--- docs/_static/t9_enable_otg.c
+++ docs/_static/t9_enable_otg.c
@@ -1,6 +1,12 @@
 /*
  * Enable OTG mode via max77818 i2c.
  *
+ * WSL2 Debian prerequisites:
+ *      sudo apt-get install 'gcc-arm*'
+ *
+ * Build:
+ *      arm-linux-gnueabi-gcc -Wall t9_enable_otg.c -o enable_otg
+ *
  * Resources:
  *      https://www.reddit.com/r/RemarkableTablet/comments/k6ir7s/experiencing_issue_with_charging_my_remarkable_2/
  *      https://www.reddit.com/user/Andrey_Dmitriev/
@@ -56,13 +62,13 @@ int main(void) {
     if ((i2cFile = open("/dev/i2c-5", O_RDWR)) < 0) printf("open failed: %d\n", i2cFile);
     printf("Opened\n");
 
-    if ((ret = ioctl(i2cFile, I2C_SLAVE_FORCE, 0x36)) < 0) printf("ioctl failed: %d\n", ret);
+    if ((ret = ioctl(i2cFile, I2C_SLAVE_FORCE, 0x69)) < 0) printf("ioctl failed: %d\n", ret);
     printf("ioctl init done\n");
 
-    ret = max77818_get_i2c_regU16(i2cFile, 0x36, 0x02, &valU16, &lsbU8, &msbU8);
+    ret = max77818_get_i2c_regU16(i2cFile, 0x69, 0xb7, &valU16, &lsbU8, &msbU8);
     printf("get done\n");
 
-    printf("Read temp alert Register %d ret = %d, valU16=%d, lsbU8=%d, msbU8=%d\n", 0x02, ret, valU16, lsbU8, msbU8);
+    printf("Read temp alert Register %d ret = %d, valU16=%d, lsbU8=%d, msbU8=%d\n", 0xb7, ret, valU16, lsbU8, msbU8);
 
     close(i2cFile);
     printf("Closed\n");
