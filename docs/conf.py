4c6eb64d8ec6ef7fd9eee15d49f5356342cfde59
Robpol86 <robpol86@gmail.com>
2021-12-07 13:24:31 -0800

    Split custom.css, resume link style.
    Made resume link style consistent with the other links within the
    admonition in index.md.
    
    Split custom.css into two scoped files.
    
    Reordered Tutorials TOC list.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ html_context = {
     ),
 }
 html_copy_source = False
-html_css_files = ["custom.css"]
+html_css_files = ["background_image.css", "fixes.css"]
 html_extra_path = [
     ".htaccess",
     "robots.txt",
@@ -93,6 +93,9 @@ myst_enable_extensions = [
     "substitution",
     "tasklist",
 ]
+myst_substitutions = {
+    "resume_link": f"[Résumé]({html_baseurl.rstrip('/')}/{html_static_path[0].strip('/')}/resume.pdf)",
+}
 myst_url_schemes = ["http", "https", "mailto"]
 
 
