e23d80b0436e11d55325fda682f7d41979c69bd9
Robpol86 <robpol86@gmail.com>
2022-05-16 14:58:55 -0500

    Disabling GitHub page link locally. (#145)
    * Disabling GitHub page link locally.
    
    Only show link in CI.
    
    * simplify license
    
    * Partially revert
    
    * oops
    
    * Version

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,10 @@ import os
 import time
 from urllib.parse import urlparse
 
+from robpol86_com import __license__, __version__ as version
 
-GIT_BRANCH = os.environ.get("SPHINX_GITHUB_BRANCH", "") or os.environ.get("GITHUB_REF", "").split("/", 2)[-1] or "main"
-GIT_URL = "https://github.com/Robpol86/robpol86.com"
+GIT_BRANCH = os.environ.get("SPHINX_GITHUB_BRANCH", "") or os.environ.get("GITHUB_REF", "").split("/", 2)[-1] or None
+GIT_URL = f'https://github.com/{os.environ["GITHUB_REPOSITORY"]}' if "GITHUB_REPOSITORY" in os.environ else None
 
 
 # General configuration.
@@ -29,6 +30,7 @@ extensions = [
 language = "en"
 project = "Robpol86.com"
 pygments_style = "vs"
+release = version
 templates_path = ["_templates"]
 
 
@@ -75,7 +77,7 @@ html_theme_options = {
         'Generator: <a href="https://www.sphinx-doc.org/">Sphinx</a><br>'
         'Theme: <a href="https://sphinx-book-theme.readthedocs.io/">Sphinx Book Theme</a><br>'
         'Host: <a href="https://www.nearlyfreespeech.net/">NearlyFreeSpeech.NET</a><br>'
-        f'License: <a href="{GIT_URL}/blob/{GIT_BRANCH}/LICENSE">BSD-2-Clause</a><br>'
+        f'License: <a href="{GIT_URL}/blob/{GIT_BRANCH}/LICENSE">{__license__}</a><br>'
         "</p>"
     ),
     "logo_only": True,
