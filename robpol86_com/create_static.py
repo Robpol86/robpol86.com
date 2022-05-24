4fe561f80bb4ca10d2a3b9b23b32be60da5d640f
Robpol86 <robpol86@gmail.com>
2022-05-24 17:58:01 -0500

    direct access

--- robpol86_com/create_static.py
+++ robpol86_com/create_static.py
@@ -1,25 +1,37 @@
 """Generate static files from templates."""
 import os
-from typing import Dict, List, Tuple
+from typing import List
 
 from sphinx.application import Sphinx
+from sphinx.builders.html import StandaloneHTMLBuilder
 
 FILES = [
     "robots.txt",
 ]
 
 
-def create_files(_) -> List[Tuple[str, Dict, str]]:
+def create_files(app: Sphinx) -> List:
     """Create files and save to root of Sphinx outdir.
 
-    :return: Page name, supplemental Jinja2 context, and template file name.
+    Calling builder.handle_page() directly to access outfilename argument.
+
+    :param app: Sphinx application.
     """
-    return [(os.path.splitext(f)[0], {}, f) for f in FILES]
+    builder: StandaloneHTMLBuilder = app.builder
+    for template_name in FILES:
+        page_name = os.path.splitext(template_name)[0]
+        builder.handle_page(
+            pagename=page_name,
+            addctx={},
+            templatename=template_name,
+            # outfilename=template_name,
+        )
+    return []
 
 
 def setup(app: Sphinx):
     """Called by Sphinx.
 
-    :param app: Sphinx application object.
+    :param app: Sphinx application.
     """
     app.connect("html-collect-pages", create_files)
