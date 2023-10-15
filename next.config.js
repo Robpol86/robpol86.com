cadff96d425539933e8546e2846ee91d2761b3e1
Robpol86 <robpol86@gmail.com>
2023-10-15 12:09:36 +0900

    configs

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-blog',
+    themeConfig: './theme.config.jsx'
+})
+
+module.exports = withNextra()
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
+
+// https://nextra.site/docs/blog-theme/start
