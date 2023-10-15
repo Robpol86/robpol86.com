19036ff53b1371afa56e3d60b1b4078feae45620
Robpol86 <robpol86@gmail.com>
2023-10-15 12:10:55 +0900

    prune old

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-"""pytest conftest."""
-import pytest
-from sphinx import __display_version__
-
-
-@pytest.hookimpl(trylast=True)
-def pytest_report_header(*_):
-    """Include library versions in output before tests start."""
-    return f"libraries: Sphinx-{__display_version__}"
