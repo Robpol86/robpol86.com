67bfb2ce066cb9d7bac403ba1e4fc0aed60cc596
Robpol86 <robpol86@gmail.com>
2023-10-16 11:59:59 +0900

    packages json

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: ['@theguild'],
+  overrides: [
+    {
+      files: '*.md{,x}',
+      parser: 'eslint-mdx',
+      processor: 'mdx/remark',
+      plugins: ['mdx'],
+      rules: {
+        'mdx/remark': 'error',
+      },
+      parserOptions: {
+        ecmaVersion: 13,
+        sourceType: 'module',
+      },
+    },
+    {
+      files: 'website/**',
+      extends: '@theguild/eslint-config/react',
+      rules: {
+        // enable without breaking existing iframes
+        'react/iframe-missing-sandbox': 'off',
+      },
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: 'website/tsconfig.json',
+          },
+        },
+      },
+    },
+    {
+      files: 'packages/website-router/**',
+      rules: {
+        'import/extensions': 'off',
+      },
+    },
+    {
+      files: 'packages/website-helper-worker/**',
+      rules: {
+        'import/extensions': 'off',
+      },
+    },
+  ],
+};
