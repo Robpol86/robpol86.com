5e966c53e6b10d0ef9fbc42e6845b7c34855ad7a
Robpol86 <robpol86@gmail.com>
2024-07-12 12:46:36 -0500

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,8 @@ source_suffix = {
     ".md": "markdown",
 }
 exclude_patterns = ["_build", "docs/manual/.ipynb_checkpoints"]
-html_css_files = ["ablog_book_theme_fix.css"]
+html_static_path = ["_static"]
+html_css_files = ["_static/ablog_book_theme_fix.css"]
 html_title = "ABlog"
 html_use_index = True
 html_domain_indices = False
