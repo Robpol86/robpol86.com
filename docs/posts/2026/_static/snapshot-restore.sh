a66eaf88185119f3c508165fb86a3adb6f3dfa6f
Robpol86 <robpol86@gmail.com>
2026-07-24 22:10:25 +0300

    todo

--- docs/posts/2026/_static/snapshot-restore.sh
+++ docs/posts/2026/_static/snapshot-restore.sh
@@ -5,3 +5,7 @@
 # Save as (chmod +x): /usr/lib/dracut/modules.d/82snapshot/snapshot-restore.sh
 
 echo "Hello World Restore"
+
+# TODOs:
+# - No no args or bad name list available snapshots
+# -     To save typing in rd.break maybe prefix each snapshot with a number and let user specify snapshot-restore -1
