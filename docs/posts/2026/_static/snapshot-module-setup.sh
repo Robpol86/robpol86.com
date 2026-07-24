b80b5da2b2c975f8002168847849f3cf61724b4a
Robpol86 <robpol86@gmail.com>
2026-07-24 19:02:20 +0300

    dracut

---
+++ docs/posts/2026/_static/snapshot-module-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# Copy snapshot-take and snapshot-restore into initramfs image.
+# https://github.com/Robpol86/robpol86.com/blob/main/docs/posts/2026/_static/snapshot-module-setup.sh
+# Save as (chmod +x): /usr/lib/dracut/modules.d/82snapshot/module-setup.sh
+#
+
+check() {
+    return 0
+}
+
+depends() {
+    echo btrfs
+    return 0
+}
+
+install() {
+    inst_multiple /sbin/snapshot-take /sbin/snapshot-restore
+}
