1fcefed99543f3a476008cf27d9f7cda6129bdff
Robpol86 <robpol86@gmail.com>
2026-07-25 14:09:11 +0300

    wip usage

--- docs/posts/2026/_static/snapshot-take.sh
+++ docs/posts/2026/_static/snapshot-take.sh
@@ -1,8 +1,27 @@
 #!/bin/sh -e
 
-# TODO.
 # https://github.com/Robpol86/robpol86.com/blob/main/docs/posts/2026/_static/snapshot-take.sh
 # Save as (chmod +x): /sbin/snapshot-take
+
+# Usage: snapshot-take [OPTIONS] SNAPSHOT_NAME
+#
+# Take named BTRFS snapshots with comments.
+#
+# TODO Long description goes here. Usually you explain what each of the
+# positional arguments do.
+#
+# Options:
+#   -c comment  Snapshot description. If comment is '-' then comment will be
+#               read from stdin.
+#   -d          TODO.
+#   -h          Display this help and exit.
+#   -l          List existing snapshots and exit. SNAPSHOT_NAME argument will be
+#               ignored.
+#   -p          Create snapshots directories as required. If this option is not
+#               specified, the full path prefix of the snapshots directory must
+#               already exist.
+#   -s          TODO.
+#   -v          Enable verbose output.
 
 echo "Hello World Take"
 
@@ -11,6 +30,7 @@ echo "Hello World Take"
 #       mount -oremount,rw /sysroot
 #       btrfs subvolume snapshot -r /sysroot /sysroot/snapshots/root-p
 # - @root and @home: can snapshots live in other subvols? Probably not.
+#   - Support non-root (arbitrary) subvolumes
 # - Metadata: maybe let users specify a comment like in VMware? Save it in a file before snapshotting?
 #   - Also store date
 #   - Allow EOF for multi-line comment. Maybe stdin?
