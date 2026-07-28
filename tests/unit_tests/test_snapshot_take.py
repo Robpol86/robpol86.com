22280af21df91eec1cb583cc20aed294ad93e2ba
Robpol86 <robpol86@gmail.com>
2026-07-28 12:43:13 +0300

    test_help_and_no_or_bad_args

---
+++ tests/unit_tests/test_snapshot_take.py
@@ -0,0 +1,34 @@
+"""Test snapshot-take.sh."""
+
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+import pytest
+
+SNAPSHOT_TAKE_PATH = Path(__file__).parent / ".." / ".." / "docs" / "posts" / "2026" / "_static" / "snapshot-take.sh"
+
+
+@pytest.fixture(autouse=True)
+def setup(monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
+    """Mock out environment and commands with fake scripts."""
+    mock_bin_dir = tmp_path / "bin"
+    mock_bin_dir.mkdir()
+    monkeypatch.setenv("PATH", str(mock_bin_dir), prepend=os.pathsep)
+
+    # macOS GNU alternatives.
+    if ggrep := shutil.which("ggrep"):
+        (mock_bin_dir / "grep").symlink_to(ggrep)
+    if gsed := shutil.which("gsed"):
+        (mock_bin_dir / "sed").symlink_to(gsed)
+
+
+def test_help_and_no_or_bad_args():
+    """Test script's handling of -h and bad CLI arguments."""
+    # Test -h.
+    output = subprocess.check_output([SNAPSHOT_TAKE_PATH, "-h"], stderr=subprocess.STDOUT)  # noqa: S603
+    lines = output.splitlines()
+    assert lines[0].startswith(b"Usage:")
+    assert lines[-2].startswith(b"  -v ")
+    assert lines[-1] == b""
