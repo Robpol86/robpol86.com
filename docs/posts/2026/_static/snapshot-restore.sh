ca1fc3244bea9c987cadfe958eb0185bcd8b4db8
Robpol86 <robpol86@gmail.com>
2026-07-29 18:18:13 +0300

    todo

--- docs/posts/2026/_static/snapshot-restore.sh
+++ docs/posts/2026/_static/snapshot-restore.sh
@@ -9,3 +9,4 @@ echo "Hello World Restore"
 # TODOs:
 # - No no args or bad name list available snapshots
 #   - To save typing in rd.break maybe prefix each snapshot with a number and let user specify snapshot-restore -1
+# - Test take+restore multiple times, creating complex nesting.
