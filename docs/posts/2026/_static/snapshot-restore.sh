7119c80a9f814322986fc36b5fc46011c6c4ecb4
Robpol86 <robpol86@gmail.com>
2026-07-30 16:43:56 +0300

    todo

--- docs/posts/2026/_static/snapshot-restore.sh
+++ docs/posts/2026/_static/snapshot-restore.sh
@@ -10,3 +10,5 @@ echo "Hello World Restore"
 # - No no args or bad name list available snapshots
 #   - To save typing in rd.break maybe prefix each snapshot with a number and let user specify snapshot-restore -1
 # - Test take+restore multiple times, creating complex nesting.
+# - Restore: mv nfo to .restored.17234567899
+#   - If user makes a new snapshot from a restored one we don’t want a collision.
