07d1cdc3ca3388c28810da6c61b47de662e04893
Robpol86 <robpol86@gmail.com>
2026-08-08 11:36:12 +0200

    todo

--- docs/posts/2026/_static/btrfs-snapshot-module-setup.sh
+++ docs/posts/2026/_static/btrfs-snapshot-module-setup.sh
@@ -30,5 +30,5 @@ install() {
     sed -i \
         -e '/^SUBVOLUME_DIR=.*@MODULE-SETUP-REPLACE@/ s|=.*|=/sysroot|' \
         "${initdir:?}/sbin/btrfs-snapshot"
-    ln -s btrfs-snapshot "${initdir:?}/sbin/bss"
+    ln -s btrfs-snapshot "${initdir:?}/sbin/bss"  # TODO sed instead of symlink.
 }
--- docs/posts/2026/_static/btrfs-snapshot.sh
+++ docs/posts/2026/_static/btrfs-snapshot.sh
@@ -391,13 +391,13 @@ fi
 
 # Subcommand restore.
 if [ "$SUBCOMMAND" = "restore" ]; then
-  set -x
   SNAPSHOT_ID="$1"  # TODO
   SUBVOLUME_DEV="$(findmnt -nvo SOURCE "$SUBVOLUME_DIR")"
   mount -oremount,rw "$SUBVOLUME_DIR"
   mkdir -p "$SUBVOLUME_DIR/.bsnaps/restore-from"
   mount -o "subvolid=$SNAPSHOT_ID,ro" "$SUBVOLUME_DEV" "$SUBVOLUME_DIR/.bsnaps/restore-from"
   btrfs subvolume snapshot "$SUBVOLUME_DIR/.bsnaps/restore-from" "$SUBVOLUME_DIR/.bsnaps/restored-$UUID"
+  # TODO: prompt user before making changes
   btrfs subvolume set-default "$SUBVOLUME_DIR/.bsnaps/restored-$UUID"
   NEW_ID="$(btrfs subvolume get-default "$SUBVOLUME_DIR" |awk '/^ID /{print $2}')"
   umount "$SUBVOLUME_DIR/.bsnaps/restore-from"
