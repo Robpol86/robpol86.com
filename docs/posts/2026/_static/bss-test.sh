683a81d906d1c9301ddc7a6a01a431077bda842c
Robpol86 <robpol86@gmail.com>
2026-08-12 17:51:17 +0200

    bss test start, loop

--- docs/posts/2026/_static/bss-test.sh
+++ docs/posts/2026/_static/bss-test.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-lastid="$(bss list |awk 'END{print $1}')";
-name="$(date +%s)";
-bss restore -f "$lastid";
-bss take "$name";
+set -eu
+bss take "$(date +%s)-start"
+for _ in 1 2 3 4 5 6 7 8 9; do
+    lastid="$(bss list |awk 'END{print $1}')";
+    name="$(date +%s)";
+    bss restore -f "$lastid";
+    bss take "$name";
+done
