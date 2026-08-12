8fc4e45ee26ff7ce894a2359cb666891c0485e50
Robpol86 <robpol86@gmail.com>
2026-08-12 21:25:07 +0200

    todo

--- docs/posts/2026/_static/btrfs-snapshot.sh
+++ docs/posts/2026/_static/btrfs-snapshot.sh
@@ -567,6 +567,7 @@ exit 1
 # - base64 security: filter out non a-z?
 # - Tell user which snapshot they're running from with restore file.
 #   - When restoring, create file that says "restored from ID". Then bss list reads that file and adds a note below said snap
+#   - Maybe instead, bring back snapshot.nfo. Before taking snapshot write its name into that file.
 # TODO:
 # - bss list -v: outputs entire awk program. Hide awk program from set -x.
 # - Consistent punctuation in echos.
