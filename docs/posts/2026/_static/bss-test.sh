b982bf7f82a3c42346b98ca28706ad2b1f119ce5
Robpol86 <robpol86@gmail.com>
2026-08-12 19:13:01 +0200

    4047+ BROKE

--- docs/posts/2026/_static/bss-test.sh
+++ docs/posts/2026/_static/bss-test.sh
@@ -6,8 +6,8 @@ read -r uuid < /proc/sys/kernel/random/uuid
 for _ in 1 2 3 4; do
     comment="$uuid $comment"
 done
-for n in 1 2; do
-for i in 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P; do
+for n in 1 2 3; do
+for i in 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M; do
     echo "$n:$i"
     lastid="$(bss list |awk 'END{print $1}')";
     name="$(date +%s)";
