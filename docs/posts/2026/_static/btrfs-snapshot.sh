3ad43de0571523a90e7312c805dd962ac185369a
Robpol86 <robpol86@gmail.com>
2026-08-13 13:25:55 +0200

    todo

--- docs/posts/2026/_static/btrfs-snapshot.sh
+++ docs/posts/2026/_static/btrfs-snapshot.sh
@@ -569,6 +569,7 @@ exit 1
 # - Tell user which snapshot they're running from with restore file.
 #   - When restoring, create file that says "restored from ID". Then bss list reads that file and adds a note below said snap
 #   - Maybe instead, bring back snapshot.nfo. Before taking snapshot write its name into that file.
+# - umask at beginning of script so all tmp txt files are 600 owned by root.
 # TODO:
 # - bss list -v: outputs entire awk program. Hide awk program from set -x.
 # - Consistent punctuation in echos.
